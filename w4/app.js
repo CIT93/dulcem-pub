9b410798e723fbbe4c9fc2a5847b7b4b88a42261
Dulce Madrid <dgmadrid1@my.scccd.edu>
2025-09-03 14:02:20 -0700

    After a refresher in old code, I figured out by help from w3Schools & previous work

--- w4/app.js
+++ w4/app.js
@@ -1,5 +1,23 @@
+// 
+console.log('JS is connected and running');
+
+const coolTimeform = document.getElementById(`coolTime`);
+const coolTimeNameinput = document.getElementById(`coolTimeName`);
+const coolStatementoutput = document.getElementById(`coolStatement`);
 
 
-const fallTime = function (event){
+
+const coolSubmit = function(event){
     event.preventDefault();
+    console.log(event);
+    const name = coolTimeNameinput.value;
+    let message = `${name} Alright, alright, alright, alright, alright`;
+    coolStatementoutput.textContent = message;
 }
+
+const init = function () {
+    console.log('App Initialized: DOM is ready! Re-submit');
+    coolTimeform.addEventListener(`submit`, coolSubmit);
+}
+
+document.addEventListener('DOMContentLoaded', init);
